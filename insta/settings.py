00a57b39486f3c57ef530d0cb5d1b0906e5a62df
John-Kimani <kimwaweru100@gmail.com>
2022-04-05 01:21:52 +0300

    Fix: Allowed host from heroku

--- insta/settings.py
+++ insta/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 #images
 cloudinary.config(
