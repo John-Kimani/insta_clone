13700977779c5438a4dbddccb0ef80a6f2c6170e
John-Kimani <kimwaweru100@gmail.com>
2022-04-05 15:45:22 +0300

    Dev: Tests photos and comments class

--- instagram/tests.py
+++ instagram/tests.py
@@ -23,3 +23,23 @@ class PostTestClass(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.post, Post))
+
+
+class CommentsTestClass(TestCase):
+    '''
+    Test class that handles comments model
+    '''
+    def setUp(self):
+        self.comment = Comments(message='A spirit from Kenya toast to the world')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment, Comments))
+
+class PhotosTestClass(TestCase):
+    '''
+    Test class that handle comments model 
+    '''
+    def setUp(self):
+        self.photo = Photos(caption='Feels great to be a web developer')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo, Photos))
