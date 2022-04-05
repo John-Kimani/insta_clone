a228a6fd59c61d151fb223854f667b70632568a2
John-Kimani <kimwaweru100@gmail.com>
2022-04-05 15:39:24 +0300

    Dev: Test post and profile model

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Comments, Profile, Photos, Post
 
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    '''
+    Test class that the profile model
+    '''
+    def setUp(self):
+        self.profile = Profile(bio='more apples')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+
+class PostTestClass(TestCase):
+    '''
+    Test class that handles post model
+    '''
+    def setUp(self):
+        self.post = Post(title='Lawn More')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
